d056ece21aa4d8368c905d15215a5d2d332d5634
Mlie <emipa606@users.noreply.github.com>
2025-01-15 20:59:35 +0100

    Fixed some missing translation-strings Used DeepL to update translations for ChineseSimplified, French, German

--- Source/WaterFreezes/HarmonyPatches/MouseoverReadout_MouseoverReadoutOnGUI.cs
+++ Source/WaterFreezes/HarmonyPatches/MouseoverReadout_MouseoverReadoutOnGUI.cs
@@ -55,20 +55,17 @@ public class MouseoverReadout_MouseoverReadoutOnGUI
         var ice = comp.IceDepthGrid[ind];
         var water = comp.WaterDepthGrid[ind];
         var naturalWater = comp.NaturalWaterTerrainGrid[ind] != null;
-        //string allWater = comp.AllWaterTerrainGrid[ind]?.defName ?? "null";
-        //float pseudoElevation = comp.PseudoWaterElevationGrid[ind];
-        //string underTerrain = comp.map.terrainGrid.UnderTerrainAt(cell)?.defName ?? "null";
         if (ice > 0)
         {
             Widgets.Label(new Rect(BotLeft.x, UI.screenHeight - BotLeft.y - rectY, 999f, 999f),
-                $"Ice depth {Math.Round(ice, 4)}");
+                "WFM.icedepth".Translate(Math.Round(ice, 4)));
             rectY += 19f;
         }
 
         if (water > 0)
         {
             Widgets.Label(new Rect(BotLeft.x, UI.screenHeight - BotLeft.y - rectY, 999f, 999f),
-                $"Water depth {Math.Round(water, 4)}");
+                "WFM.waterdepth".Translate(Math.Round(water, 4)));
             rectY += 19f;
         }
 
@@ -77,18 +74,9 @@ public class MouseoverReadout_MouseoverReadoutOnGUI
             return rectY;
         }
 
-        Widgets.Label(new Rect(BotLeft.x, UI.screenHeight - BotLeft.y - rectY, 999f, 999f), "Natural Water");
+        Widgets.Label(new Rect(BotLeft.x, UI.screenHeight - BotLeft.y - rectY, 999f, 999f),
+            "WFM.naturalwater".Translate());
         rectY += 19f;
-
-        //Widgets.Label(new Rect(BotLeft.x, (float)UI.screenHeight - BotLeft.y - rectY, 999f, 999f), "UnderTerrain: " + underTerrain);
-        //rectY += 19f;
-        //Widgets.Label(new Rect(BotLeft.x, (float)UI.screenHeight - BotLeft.y - rectY, 999f, 999f), allWater);
-        //rectY += 19f;
-        //if (pseudoElevation > 0)
-        //{
-        //    Widgets.Label(new Rect(BotLeft.x, (float)UI.screenHeight - BotLeft.y - rectY, 999f, 999f), "Elevation " + pseudoElevation.ToString());
-        //    rectY += 19f;
-        //}
         return rectY;
     }
 }
